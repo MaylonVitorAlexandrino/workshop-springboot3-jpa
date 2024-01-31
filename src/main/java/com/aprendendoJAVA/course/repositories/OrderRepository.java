ce4e259216adfcc3eec53d80e92a26b4b0df1b50
Maylon Vitor Alexandrino <maylonvitor110283@gmail.com>
2024-01-31 16:45:59 -0300

    Order, Instant, ISO 8601 PART 2

---
+++ src/main/java/com/aprendendoJAVA/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.aprendendoJAVA.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.aprendendoJAVA.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
