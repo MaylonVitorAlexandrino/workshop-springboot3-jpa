2c7c778b290138d1ed8ef8af719577e61b45cfa1
Maylon Vitor Alexandrino <maylonvitor110283@gmail.com>
2024-02-01 12:33:51 -0300

    Product

---
+++ src/main/java/com/aprendendoJAVA/course/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.aprendendoJAVA.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.aprendendoJAVA.course.entities.Product;
+import com.aprendendoJAVA.course.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll(){
+		return repository.findAll();
+	}
+
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+}
