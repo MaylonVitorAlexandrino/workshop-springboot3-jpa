a3886ce98b001d6fc4191206d4d0f0347b4d4ccb
Maylon Vitor Alexandrino <maylonvitor110283@gmail.com>
2024-02-02 11:53:25 -0300

    Payment, one-to-one association

--- src/main/java/com/aprendendoJAVA/course/config/TestConfig.java
+++ src/main/java/com/aprendendoJAVA/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.aprendendoJAVA.course.entities.Category;
 import com.aprendendoJAVA.course.entities.Order;
 import com.aprendendoJAVA.course.entities.OrderItem;
+import com.aprendendoJAVA.course.entities.Payment;
 import com.aprendendoJAVA.course.entities.Product;
 import com.aprendendoJAVA.course.entities.User;
 import com.aprendendoJAVA.course.entities.enums.OrderStatus;
@@ -79,6 +80,11 @@ public void run(String... args) throws Exception {
 	OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 	
 	orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+	
+	Payment payment = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+	o1.setPayment(payment);
+	
+	orderRepository.save(o1);
 }
 
 }
